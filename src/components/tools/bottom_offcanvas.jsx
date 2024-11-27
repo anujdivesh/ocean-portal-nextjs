19f4e0ef02f30b5b5819e9be99ce6fe00878dfaf
Anuj Divesh <divesha@spc.int>
2024-11-28 11:52:16 +1200

    added tabs

--- src/components/tools/bottom_offcanvas.jsx
+++ src/components/tools/bottom_offcanvas.jsx
@@ -5,6 +5,10 @@ import { showoffCanvas, hideoffCanvas  } from '@/app/GlobalRedux/Features/offcan
 import { useAppSelector, useAppDispatch } from '@/app/GlobalRedux/hooks';
 import dynamic from 'next/dynamic';
 import 'chart.js/auto';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+
+
 const Line = dynamic(() => import('react-chartjs-2').then((mod) => mod.Line), {
   ssr: false,
 });
@@ -28,16 +32,26 @@ function BottomOffCanvas({isVisible}) {
         dispatch(hideoffCanvas())
       };
 return(
-    <Offcanvas show={isVisible} onHide={handleClose} placement="bottom" className="offcanvas-bottom" backdrop={false} scroll={true}>
-    <Offcanvas.Header closeButton>
-      <Offcanvas.Title>Timeseries viewer</Offcanvas.Title>
-    </Offcanvas.Header>
-    <Offcanvas.Body>
-    <div style={{ width: '100%', height: '200px' }}>
-            <Line data={data} options={{ maintainAspectRatio: false }} />
-          </div>
-    </Offcanvas.Body>
-  </Offcanvas>
+  <Offcanvas show={isVisible} onHide={handleClose} placement="bottom" className="offcanvas-bottom" backdrop={false} scroll={true}>
+  <Offcanvas.Header closeButton >
+    <Offcanvas.Title>Utilities</Offcanvas.Title>
+  </Offcanvas.Header>
+  <Offcanvas.Body style={{paddingTop:'50'}}>
+    <Tabs  id="offcanvas-tabs">
+      <Tab eventKey="chart" title="Timeseries">
+        <div style={{ width: '100%', height: '180px' }}>
+          <Line data={data} options={{ maintainAspectRatio: false }} />
+        </div>
+      </Tab>
+      <Tab eventKey="info" title="Get Map">
+        <p>This is the info tab content.</p>
+      </Tab>
+      <Tab eventKey="settings" title="Download">
+        <p>This is the settings tab content.</p>
+      </Tab>
+    </Tabs>
+  </Offcanvas.Body>
+</Offcanvas>
 )
 }
 export default BottomOffCanvas;
